52f071b351bbae8dbd8db2ed84e4743c6fc4b7d3
Chris Chi <chrischi90@gmail.com>
2017-03-04 16:57:26 -0500

    adding users and features

---
+++ homework-7/problem-1/hw7-problem1-app/db/migrate/20170304211542_add_fields_to_user_model.rb
@@ -0,0 +1,7 @@
+class AddFieldsToUserModel < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :headline, :string
+    add_column :users, :admin, :boolean, default: false
+  end
+end
